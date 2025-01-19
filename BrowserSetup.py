5f3d18b49f4a7d643da69276b1714ad63268fd01
Diego Martins <esmartdie@gmail.com>
2025-01-19 17:17:53 +0100

    adding license

--- BrowserSetup.py
+++ BrowserSetup.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2025 Diego Martins
+# Licensed under the MIT License. See LICENSE file in the project root for details.
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
